998cec679e44ac83faada04e8b35fd5b720c1fe1
ArbiterAbhishek <mishra.ishere@gmail.com>
2018-09-12 01:42:25 +0530

    Create basic.bat
    basic.bat file

---
+++ opt_out_exam/Mishra_Abhishek/IT/basic.bat
@@ -0,0 +1,14 @@
+@echo off
+
+cd opt_out_exam
+md Mishra-Abhishek 
+cd Mishra-Abhishek
+md IT
+md Maths
+cd IT
+md made_by_my_script
+echo Hello world
+
+pause
+cd made_by_my_script
+start notepad hello.txt
